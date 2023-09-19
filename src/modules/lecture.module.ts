ebdf84cb644f18216a614a4b1dbe10eccfea8b0e
unknown <codeing999@gmail.com>
2023-09-19 18:07:21 +0900

    init

--- src/modules/lecture.module.ts
+++ src/modules/lecture.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LectureController } from '../controllers/lecture.controller';
 import { LectureService } from '../services/lecture.service';
-import { LectureDto } from '../dtos/lecture.dto';
+import { LectureProvider } from '../providers/lecture.provider';
 
 @Module({
   controllers: [LectureController],
-  providers: [LectureService, LectureDto],
+  providers: [LectureService, LectureProvider],
 })
 export class LectureModule {}
